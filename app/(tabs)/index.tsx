fc42e5f093ee2e40e8f7a078256c94c1d0803667
sahad-cp <sahadmanu4@gmail.com>
2024-07-16 15:22:03 +0530

    ready to push

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,70 +1,94 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { StyleSheet, Text, View ,Image, TouchableOpacity, TextInput} from 'react-native'
+import React from 'react'
+import { Stack } from 'expo-router'
+import { Ionicons } from '@expo/vector-icons'
+import Colors from '@/constants/Colors'
+import {useHeaderHeight} from '@react-navigation/elements'
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
 
-export default function HomeScreen() {
+
+
+const Page = () => {
+  const headerHeight = useHeaderHeight();
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
-  );
+    <>
+      <Stack.Screen options={{
+        headerTransparent:true,
+        headerTitle:"",
+       headerLeft:()=>(
+        <TouchableOpacity style={{marginLeft:20}} >
+          <Image 
+          source={{uri:'https://c0.lestechnophiles.com/www.numerama.com/wp-content/uploads/2023/01/cat-midjourney.jpg?key=13f0611b'}}
+          style={{width:40,height:40,borderRadius:10}}
+          />
+        </TouchableOpacity>
+       ),
+       headerRight:()=>(
+        <TouchableOpacity style={{
+          marginRight:20,
+          backgroundColor:Colors.white,
+          padding:10,
+          borderRadius:10,
+          shadowColor:'#171717',
+          shadowOffset:{width:2,height:4},
+          shadowOpacity:0.2,
+          shadowRadius:3
+          }} >
+        <Ionicons name='notifications' size={20} color={Colors.black}/>
+      </TouchableOpacity>
+       )
+      }}/>
+      <View style={[styles.container,{paddingTop:headerHeight}]}>
+        <Text style={styles.headingTxt}>Explore The Beautiful World!</Text>
+        <View style={styles.searchSectionWrapper}>
+          <View style={styles.searchBar}>
+            <Ionicons name='search' 
+            size={18}
+            style={{marginRight:5}}
+            color={Colors.black}
+            />
+           <TextInput placeholder='Search...'/>
+          </View>
+          <TouchableOpacity onPress={()=>{}} style={styles.filterBtn}>
+            <Ionicons name='options' size={28} color={Colors.white} />
+          </TouchableOpacity>
+        </View>
+      </View>
+    </>
+  )
 }
 
+export default Page
+
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
-});
+    container:{
+        flex:1,
+        backgroundColor:Colors.bgColor,
+        paddingHorizontal:20,
+    },
+    headingTxt:{
+      fontSize:28,
+      fontWeight:'800',
+      color:Colors.black,
+      marginTop:10
+    },
+    searchSectionWrapper:{
+      flexDirection:'row',
+      marginVertical:20,
+
+    },
+    searchBar:{
+      flex:1,
+      flexDirection:'row',
+      backgroundColor:Colors.white,
+      padding:16,
+      borderRadius:10,
+
+    },
+    filterBtn:{
+      backgroundColor: Colors.primaryColor,
+      padding:12,
+      borderRadius:10,
+      marginLeft:10
+    }
+})
